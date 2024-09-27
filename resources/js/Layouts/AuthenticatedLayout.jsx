b0c3dc9ed0db6e79d8f871d210b3fa0f6a6defbc
chaimaela <laaguidichaimae@gmail.com>
2024-09-27 23:47:42 +0100

    create controllers and routes

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -26,6 +26,18 @@ export default function Authenticated({ header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+
+                                <NavLink href={route('project.index')} active={route().current('project.index')}>
+                                    Projects
+                                </NavLink>
+
+                                <NavLink href={route('task.index')} active={route().current('task.index')}>
+                                    All tasks
+                                </NavLink>
+
+                                <NavLink href={route('user.index')} active={route().current('user.index')}>
+                                    Users
+                                </NavLink>
                             </div>
                         </div>
 
