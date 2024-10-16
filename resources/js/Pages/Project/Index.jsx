d28cd78b97d659e0ab363b90d5b64461924fc15a
chaimaela <laaguidichaimae@gmail.com>
2024-10-16 23:30:26 +0100

    feat: deleting and updating projects

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -42,28 +42,41 @@ export default function index({ auth, projects, queryParams = null, success }) {
     router.get(route("project.index"), queryParams, { preserveState: true });
   };
 
+
+  const deleteProject= (project) =>{
+    if(!window.confirm("Are you sure you want to delete the project?")){
+return;
+    }
+
+    router.delete(route('project.destroy', project.id))
+  };
+
   return (
     <AuthenticatedLayout
       user={auth.user}
       header={
-     <div className='flex justify-between items-center'>
-         <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          Projects
-        </h2>
-        <Link href={route("project.create")} className='bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600'>
-        Add New 
-        </Link>
-     </div>
+        <div className="flex justify-between items-center">
+          <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Projects
+          </h2>
+          <Link
+            href={route("project.create")}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+          >
+            Add New
+          </Link>
+        </div>
       }
     >
       <Head title="Projects" />
-     
-     
+
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        {success && ( <div className='bg-emerald-500 py-2 px-4 text-white rounded mb-4'>
-      {success}
-      </div>)}
+          {success && (
+            <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+              {success}
+            </div>
+          )}
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900 dark:text-gray-100">
               <div className="overflow-auto">
@@ -189,13 +202,12 @@ export default function index({ auth, projects, queryParams = null, success }) {
                             {" "}
                             Edit
                           </Link>
-                          <Link
-                            href={route("project.destroy", project.id)}
+                          <button
+                          onClick={e => deleteProject(project)}
                             className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
                           >
-                            {" "}
                             delete
-                          </Link>
+                          </button>
                         </td>
                       </tr>
                     ))}
